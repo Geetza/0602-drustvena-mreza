93827134932fb8e847d59f040d29ee638a927894
Fenzakosu <lukakovacevic266@gmail.com>
2025-05-26 19:59:11 +0200

    feat: KorisnikDbRepo dodat, GetById i Create metode unutar klase, izmenjen KorisnikContoller

--- 0601DrustvenaMreza/Controller/KorisnikController.cs
+++ 0601DrustvenaMreza/Controller/KorisnikController.cs
@@ -15,18 +15,13 @@ namespace _0601DrustvenaMreza.Controller
         private GrupaRepo grupaRepo = new GrupaRepo();
         private KorisnikRepo korisnikRepo = new KorisnikRepo();
 
-        //[HttpGet]
-        //public ActionResult<List<Korisnik>> GetAll()
-        //{
-        //    List<Korisnik> korisnici = KorisnikRepo.Data.Values.ToList();
-        //    return Ok(korisnici);
-        //}
+        private KorisnikDbRepo korisnikDbRepo = new KorisnikDbRepo();
 
-        // GET
+        
         [HttpGet]
         public ActionResult<List<Korisnik>> GetUsers()
         {
-            List<Korisnik> korisnici = GetAllFromDatabase();
+            List<Korisnik> korisnici = korisnikDbRepo.GetAll();
             try
             {
                 if (korisnici == null || korisnici.Count == 0)
@@ -45,60 +40,7 @@ namespace _0601DrustvenaMreza.Controller
 
 
         
-        private List<Korisnik> GetAllFromDatabase()
-        {
-                List<Korisnik> korisnici = new List<Korisnik>();
-            try
-            {
-                string dbPath = Path.Combine("database", "mydatabase.db");
-                using var connection = new SqliteConnection($"Data Source={dbPath}");
-                Console.WriteLine(dbPath);
-
-                if (!System.IO.File.Exists(dbPath))
-                {
-                    throw new FileNotFoundException($"Baza podataka ne postoji na putanji: {dbPath}");
-                }
-
-                connection.Open();
-
-                string query = "SELECT * FROM Korisnici";
-                using var command = new SqliteCommand(query, connection);
-                using var reader = command.ExecuteReader();
-
-
-                while (reader.Read())
-                {
-                    Korisnik korisnik;
-                    int id = Convert.ToInt32(reader["Id"]);
-                    string korIme = reader["KorIme"]?.ToString();
-                    string ime = reader["Ime"]?.ToString();
-                    string prezime = reader["Prezime"]?.ToString();
-                    string datumRodjenjaString = reader["DatumRodjenja"].ToString();
-                    DateTime datumRodjenja = DateTime.ParseExact(datumRodjenjaString, "yyyy-MM-dd", CultureInfo.InvariantCulture);
-                    if (id != -1 && korIme != null && ime != null && prezime != null && datumRodjenja != DateTime.MinValue)
-                    {
-                         korisnik = new Korisnik(id, korIme, ime, prezime, datumRodjenja);
-                        korisnici.Add(korisnik);
-                    } else
-                    {
-                        return korisnici;
-                    }
-                    
-                }
-
-                return korisnici;
-            }
-            catch (SqliteException ex)
-            {
-                Console.WriteLine($"SQLite error: {ex.Message}");
-                return korisnici;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Unexpected error: {ex.Message}");
-                return korisnici;
-            }
-        }
+        
 
 
 
@@ -106,43 +48,56 @@ namespace _0601DrustvenaMreza.Controller
         [HttpGet("{korisnikId}")]
         public ActionResult<Korisnik> GetById(int korisnikId)
         {
-            if (!KorisnikRepo.Data.ContainsKey(korisnikId))
-            {
-                return NotFound();
-            }
+            
 
-            return Ok(KorisnikRepo.Data[korisnikId]);
+            try
+            {
+                Korisnik korisnik = korisnikDbRepo.GetById(korisnikId);
+                if (korisnik == null)
+                {
+                    return NotFound();
+                }
+                return Ok(korisnik);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"Doslo je do greske: {ex.Message}");
+                return StatusCode(500);
+            }
         }
 
         [HttpPost]
         public ActionResult<Korisnik> Create([FromBody] Korisnik noviKorisnik)
         {
             if (string.IsNullOrWhiteSpace(noviKorisnik.KorIme) || string.IsNullOrWhiteSpace(noviKorisnik.Ime) ||
-                string.IsNullOrWhiteSpace(noviKorisnik.Prezime) || noviKorisnik.DatumRodjenja == DateTime.Now)
+                string.IsNullOrWhiteSpace(noviKorisnik.Prezime) || noviKorisnik.DatumRodjenja == DateTime.MinValue)
             {
                 return BadRequest();
             }
 
-            noviKorisnik.Id = IzracunajNoviId(KorisnikRepo.Data.Keys.ToList());
-            KorisnikRepo.Data[noviKorisnik.Id] = noviKorisnik;
-            korisnikRepo.Save();
+            noviKorisnik.Id = korisnikDbRepo.InsertNewUser(noviKorisnik);
+            if (noviKorisnik.Id == 0)
+            {
+                return BadRequest();
+            }
 
             return Ok(noviKorisnik);
         }
 
-        private int IzracunajNoviId(List<int> identifikatori)
-        {
-            int maxId = 0;
-            foreach (int id in identifikatori)
-            {
-                if (id > maxId)
-                {
-                    maxId = id;
-                }
-            }
+        //private int IzracunajNoviId(List<int> identifikatori)
+        //{
+        //    int maxId = 0;
+        //    foreach (int id in identifikatori)
+        //    {
+        //        if (id > maxId)
+        //        {
+        //            maxId = id;
+        //        }
+        //    }
 
-            return maxId + 1;
-        }
+        //    return maxId + 1;
+        //}
 
         [HttpPut("{korisnikId}")]
         public ActionResult<Korisnik> Update(int korisnikId, [FromBody] Korisnik noviKorisnik)
---
+++ 0601DrustvenaMreza/Repository/KorisnikDbRepo.cs
@@ -0,0 +1,191 @@
+﻿using _0601DrustvenaMreza.Model;
+using Microsoft.Data.Sqlite;
+using System.Globalization;
+
+namespace _0601DrustvenaMreza.Repository
+{
+    public class KorisnikDbRepo
+    {
+
+        public KorisnikDbRepo() { }
+
+        public List<Korisnik> GetAll()
+        {
+            List<Korisnik> korisnici = new List<Korisnik>();
+            try
+            {
+                string dbPath = Path.Combine("database", "mydatabase.db");
+                using var connection = new SqliteConnection($"Data Source={dbPath}");
+                Console.WriteLine(dbPath);
+
+                if (!System.IO.File.Exists(dbPath))
+                {
+                    throw new FileNotFoundException($"Baza podataka ne postoji na putanji: {dbPath}");
+                }
+
+                connection.Open();
+
+                string query = "SELECT * FROM Korisnici";
+                using var command = new SqliteCommand(query, connection);
+                using var reader = command.ExecuteReader();
+
+
+                while (reader.Read())
+                {
+                    Korisnik korisnik;
+                    int id = Convert.ToInt32(reader["Id"]);
+                    string korIme = reader["KorIme"]?.ToString();
+                    string ime = reader["Ime"]?.ToString();
+                    string prezime = reader["Prezime"]?.ToString();
+                    string datumRodjenjaString = reader["DatumRodjenja"].ToString();
+                    DateTime datumRodjenja = DateTime.ParseExact(datumRodjenjaString, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+                    if (id != -1 && korIme != null && ime != null && prezime != null && datumRodjenja != DateTime.MinValue)
+                    {
+                        korisnik = new Korisnik(id, korIme, ime, prezime, datumRodjenja);
+                        korisnici.Add(korisnik);
+                    }
+                    else
+                    {
+                        return korisnici;
+                    }
+
+                }
+
+                return korisnici;
+            }
+            catch (SqliteException ex)
+            {
+                Console.WriteLine($"Greška pri konekciji ili izvršavanju neispravnih SQL upita: {ex.Message}");
+                throw;
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine($"Greška u konverziji podataka iz baze: {ex.Message}");
+                throw;
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine($"Konekcija nije otvorena ili je otvorena više puta: {ex.Message}");
+                throw;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Neočekivana greška: {ex.Message}");
+                throw;
+            }
+        }
+
+        public Korisnik GetById(int idKorisnika)
+
+
+        {
+
+
+            try
+            {
+                string dbPath = Path.Combine("database", "mydatabase.db");
+                using var connection = new SqliteConnection($"Data Source={dbPath}");
+
+                if (!System.IO.File.Exists(dbPath))
+                {
+                    throw new FileNotFoundException($"Baza podataka ne postoji na putanji: {dbPath}");
+                }
+
+                connection.Open();
+
+                string query = "SELECT * FROM Korisnici WHERE Id = @Id";
+                using SqliteCommand command = new SqliteCommand(query, connection);
+
+                command.Parameters.AddWithValue("@Id", idKorisnika);
+                using SqliteDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+
+                {
+                    string korIme = reader["KorIme"]?.ToString();
+                    string ime = reader["Ime"]?.ToString();
+                    string prezime = reader["Prezime"]?.ToString();
+                    string datumRodjenjaString = reader["DatumRodjenja"].ToString();
+                    DateTime datumRodjenja = DateTime.ParseExact(datumRodjenjaString, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+
+                    return new Korisnik(idKorisnika, korIme, ime, prezime, datumRodjenja);
+                }
+                return null;
+
+            }
+            catch (SqliteException ex)
+            {
+                Console.WriteLine($"Greška pri konekciji ili izvršavanju neispravnih SQL upita: {ex.Message}");
+                throw;
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine($"Greška u konverziji podataka iz baze: {ex.Message}");
+                throw;
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine($"Konekcija nije otvorena ili je otvorena više puta: {ex.Message}");
+                throw;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Neočekivana greška: {ex.Message}");
+                throw;
+            }
+        }
+
+
+        public int InsertNewUser(Korisnik korisnik)
+        {
+            try
+            {
+                string dbPath = Path.Combine("database", "mydatabase.db");
+                using var connection = new SqliteConnection($"Data Source={dbPath}");
+
+                if (!System.IO.File.Exists(dbPath))
+                {
+                    throw new FileNotFoundException($"Baza podataka ne postoji na putanji: {dbPath}");
+                }
+
+                connection.Open();
+
+                
+                string query = @"INSERT INTO Korisnici (KorIme,Ime, Prezime,DatumRodjenja) VALUES (@KorIme,@Ime,@Prezime,@DatumRodjenja); SELECT LAST_INSERT_ROWID();";
+
+                using SqliteCommand command = new SqliteCommand(query, connection);
+
+                // Dodavanje parametara za ime i godine
+                command.Parameters.AddWithValue("@KorIme", korisnik.KorIme);
+                command.Parameters.AddWithValue("@Ime", korisnik.Ime);
+                command.Parameters.AddWithValue("@Prezime", korisnik.Prezime);
+                command.Parameters.AddWithValue("@DatumRodjenja", korisnik.DatumRodjenja.ToString("yyyy-MM-dd"));
+
+                // Izvršavanje oba upita, gde ExecuteScalar vraća id poslednjeg ubačenog reda
+                int lastInsertedId = Convert.ToInt32(command.ExecuteScalar());  // Izvršava oba upita i vraća id
+                return lastInsertedId;
+
+            }
+            catch (SqliteException ex)
+            {
+                Console.WriteLine($"Greška pri konekciji ili izvršavanju neispravnih SQL upita: {ex.Message}");
+                throw;
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine($"Greška u konverziji podataka iz baze: {ex.Message}");
+                throw;
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine($"Konekcija nije otvorena ili je otvorena više puta: {ex.Message}");
+                throw;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Neočekivana greška: {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
