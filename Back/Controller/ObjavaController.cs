f51bb5bc25a51cd5dfa16b08d5c87e1a035fbef5
Fenzakosu <lukakovacevic266@gmail.com>
2025-06-11 10:52:17 +0200

    feat: Model za objave, get metoda implementirana

---
+++ Back/Controller/ObjavaController.cs
@@ -0,0 +1,35 @@
+﻿using _0601DrustvenaMreza.Model;
+using _0601DrustvenaMreza.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _0601DrustvenaMreza.Controller
+{
+    [Route("api")]
+    [ApiController]
+    public class ObjavaController : ControllerBase
+    {
+        private readonly KorisnikDbRepo korisnikDbRepo;
+        private readonly ObjavaDBRepo objavaDBRepo;
+
+        public ObjavaController(IConfiguration configuration)
+        {
+            objavaDBRepo = new ObjavaDBRepo(configuration);
+            korisnikDbRepo = new KorisnikDbRepo(configuration);
+        }
+
+        [HttpGet("objave")]
+        public ActionResult<List<Objava>> GetAll()
+        {
+            try
+            {
+                List<Objava> objave = objavaDBRepo.GetAll();
+                return Ok(objave);
+            }
+            catch (Exception ex)
+            {
+                return Problem($"Doslo je do greske: {ex.Message}");
+            }
+        }   
+    }
+}
---
+++ Back/Repository/ObjavaDBRepo.cs
@@ -0,0 +1,103 @@
+﻿using System.Globalization;
+using _0601DrustvenaMreza.Model;
+using Microsoft.Data.Sqlite;
+
+namespace _0601DrustvenaMreza.Repository
+{
+    public class ObjavaDBRepo
+    {
+        private readonly string connectionString;
+
+        public ObjavaDBRepo(IConfiguration configuration)
+        {
+            connectionString = configuration["ConnectionString:SQLiteConnection"];
+        }
+
+        public List<Objava> GetAll()
+        {
+            List<Objava> objave = new List<Objava>();
+            try
+            {
+                using SqliteConnection connection = new SqliteConnection(connectionString);
+                connection.Open();
+
+                string query = @"SELECT 
+                                    o.Id ,
+                                    o.Sadrzaj,
+                                    o.Datum AS DatumObjave,
+                                    k.Id AS IdKorisnika,
+                                    k.KorIme,
+                                    k.Ime,
+                                    k.Prezime,
+                                    k.DatumRodjenja AS DatumRodjenja
+                                FROM Objave o
+                                LEFT JOIN Korisnici k ON o.KorisnikId = k.Id;
+                                ";
+                using SqliteCommand command = new SqliteCommand(query, connection);
+
+                using SqliteDataReader reader = command.ExecuteReader();
+
+                Objava objava = null;
+
+                while (reader.Read())
+                {
+
+
+
+                    {
+                        objava = new Objava
+                        {
+                            Id = Convert.ToInt32(reader["Id"]),
+                            Sadrzaj = reader["Sadrzaj"].ToString(),
+                            Datum = DateTime.ParseExact(reader["DatumObjave"].ToString(), "yyyy-MM-dd", CultureInfo.InvariantCulture)
+                        };
+
+
+
+
+
+
+                        objave.Add(objava);
+
+
+                        if (reader["IdKorisnika"] != DBNull.Value)
+                        {
+                            Korisnik korisnik;
+                            int id = Convert.ToInt32(reader["IdKorisnika"]);
+                            string korIme = reader["KorIme"]?.ToString();
+                            string ime = reader["Ime"]?.ToString();
+                            string prezime = reader["Prezime"]?.ToString();
+                            string datumRodjenjaString = reader["DatumRodjenja"].ToString();
+                            DateTime datumRodjenja = DateTime.ParseExact(datumRodjenjaString, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+                            korisnik = new Korisnik(id, korIme, ime, prezime, datumRodjenja);
+                            objava.Korisnik = korisnik;
+                        }
+                    }
+                }
+                return objave;
+            }
+            catch (SqliteException ex)
+            {
+                Console.WriteLine($"Greška pri konekciji ili izvršavanju neispravnih SQL upita: {ex.Message}");
+                throw;
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine($"Greška u konverziji podataka iz baze: {ex.Message}");
+                throw;
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine($"Konekcija nije otvorena ili je otvorena više puta: {ex.Message}");
+                throw;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Neočekivana greška: {ex.Message}");
+                throw;
+            }
+
+            
+        }
+    }
+}
